7bdfb2966645514de14b7ae4ba1097194fb68b21
KISHI Kousuke <ksh2ksk4@gmail.com>
2023-04-21 00:12:37 +0900

    tweak

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,19 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "sl-mozc" is now active!');
+  console.clear();
+  console.log('sl-mozc - activate(): start');
 
   let disposable = vscode.commands.registerCommand('sl-mozc.helloWorld', () => {
     vscode.window.showInformationMessage('Hello World from sl-mozc!');
   });
 
   context.subscriptions.push(disposable);
+
+  console.log('sl-mozc - activate(): end');
 }
 
-export function deactivate() {}
+export function deactivate() {
+  console.log('sl-mozc - deactivate(): start');
+  console.log('sl-mozc - deactivate(): end');
+}
