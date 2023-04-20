c8df39754c92f7088f5e33b01364a99083733cd8
KISHI Kousuke <ksh2ksk4@gmail.com>
2023-04-21 00:56:04 +0900

    Implement executing external commands for now

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,34 @@
 import * as vscode from 'vscode';
+import * as childProcess from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
   console.clear();
   console.log('sl-mozc - activate(): start');
 
   let disposable = vscode.commands.registerCommand('sl-mozc.helloWorld', () => {
-    vscode.window.showInformationMessage('Hello World from sl-mozc!');
+    const command = 'echo "(0 CreateSession)\n(1 SendKey 1 a)" | /Users/ksh2ksk4/mine/bin/mozc_emacs_helper';
+
+    /**
+     * about exec()
+     *
+     * https://stackabuse.com/executing-shell-commands-with-node-js/
+     * https://www.js-tutorials.com/nodejs-tutorial/how-to-execute-shell-command-in-nodejs/
+     */
+    childProcess.exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error('sl-mozc - error: %o', error);
+
+        return;
+      }
+
+      if (stderr) {
+        console.error('sl-mozc - stderr: %o', stderr);
+
+        return;
+      }
+
+      console.log('sl-mozc - stdout: %o', stdout);
+    });
   });
 
   context.subscriptions.push(disposable);
